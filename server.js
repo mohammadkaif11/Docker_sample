dc1a05c2e85c928da7e2288125cb351e0a7db0ba
mohammadkaif <mohammadkaif0211@gmail.com>
2022-10-04 13:16:28 +0530

    changing port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-const port=process.env.port|| 3000;
+const port=process.env.port|| 8080;
 app.get('/', function(req, res){
    res.send("Hello world!");
 });
