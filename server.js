b01feb5272beaba1951f412102797b215a367b11
mohammadkaif <83584426+mohammadkaif11@users.noreply.github.com>
2022-10-08 14:12:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 const port=process.env.port|| 8080;
 app.get('/', function(req, res){
-   res.send("Hello world!");
+   res.send("Application");
 });
 app.get('/kaif', function(req, res){
     res.send("hii sharding developer");
